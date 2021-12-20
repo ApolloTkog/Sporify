c19ee6c781687c6a73c107cbe26b8e709f67c276
metalfrider <friderikosj@gmail.com>
2021-12-20 16:51:44 +0200

    Deleted old code that is now on new classes from HomeViewModel

--- app/src/main/java/com/myapp/sporify/fragments/home/HomeViewModel.java
+++ app/src/main/java/com/myapp/sporify/fragments/home/HomeViewModel.java
@@ -29,17 +29,17 @@ import java.util.List;
 public abstract class HomeViewModel extends AndroidViewModel implements ApiService {
 
     public MutableLiveData<List<Artist>> artists;
-    private MutableLiveData<List<Track>> tracks;
+
 
     private List<Artist> artistList;
-    private List<Track> trackList;
+
 
     private RequestQueue requestQueue;
 
     public HomeViewModel(Application application ) {
         super(application);
         artistList = new ArrayList<>();
-        trackList = new ArrayList<>();
+
 
         requestQueue = VolleySingleton.getmInstance(getApplication()).getRequestQueue();
     }
@@ -129,50 +129,7 @@ public abstract class HomeViewModel extends AndroidViewModel implements ApiServi
     }
 
 
-    @Override
-    public LiveData<List<Track>> getTopTracks(){
-        if (tracks == null) {
-            tracks = new MutableLiveData<>();
-            fetchTopTracks();
-        }
-        return tracks;
-    }
 
-    private void fetchTopTracks() {
-        String url =
-                "http://ws.audioscrobbler.com/2.0/?format=json&method=chart.gettoptracks&api_key=8fc89f699e4ff45a21b968623a93ed52";
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
-            try {
-                JSONObject artists_json = response.getJSONObject("tracks");
-                JSONArray jsonArray = artists_json.getJSONArray("track");
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
 
-//                    if(jsonObject.getString("mbid").isEmpty())
-//                        continue;
-
-//                    String mbid = jsonObject.getString("mbid");
-                    String name = jsonObject.getString("name");
-                    String image = jsonObject.getJSONArray("image").getJSONObject(3).getString("#text");
-                    String artistName = jsonObject.getJSONObject("artist").getString("name");
-
-                    Track track = new Track(i + 1, name, artistName, image);
-                    trackList.add(track);
-                }
-
-                tracks.postValue(trackList);
-
-            } catch (JSONException e) {
-                Log.d("Parsing error: ", e.getMessage());
-                Toast.makeText(getApplication(), e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        }, error -> {
-            Toast.makeText(getApplication(), error.getMessage(), Toast.LENGTH_SHORT).show();
-            Log.d("Request error: ", error.getMessage());
-        });
-
-        requestQueue.add(jsonObjectRequest);
-
-    }
 }
