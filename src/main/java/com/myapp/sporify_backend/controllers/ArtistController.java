0066e32d0508cf6b1632a7c0be6460ec098271e3
delef <d.eleftheriadhs@gmail.com>
2022-01-09 22:16:39 +0200

    update artist controller

--- src/main/java/com/myapp/sporify_backend/controllers/ArtistController.java
+++ src/main/java/com/myapp/sporify_backend/controllers/ArtistController.java
@@ -32,25 +32,23 @@ public class ArtistController {
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
         String currentUseId = userDetails.getId();
 
-        // find current authenticated user
         User user = userRepository.findById(currentUseId).orElseGet(User::new);
-        // get current user's albums
         Set<Artist> userArtists = user.getArtists();
 
-        // if user's artists contains the artist we want to insert
-        // then show a message
-        if(userArtists.contains(artist)){
-            // find track that we want to be deleted
-            Artist artistFound = artistRepository.findArtistByName(artist.getName()).orElseGet(Artist::new);
 
-            // delete from albums collection
+        if(userArtists.contains(artist)){
+            Artist artistFound = new Artist();
+            for(Artist x : userArtists){
+                if(x.getName().equals(artist.getName())){
+                    artistFound = x;
+                    break;
+                }
+            }
+
             artistRepository.delete(artistFound);
 
-            // remove it from user's set
             userArtists.remove(artistFound);
 
-            // update user's track list
-            // with newly deleted track
             userRepository.save(user);
 
             return ResponseEntity.ok().body(new MessageResponse("Artist with id " + artistFound.getId() + " has been successfully deleted!"));
@@ -58,12 +56,9 @@ public class ArtistController {
 
         // set track's owner
         artist.setUserId(user);
-        // create track and insert it inside collection
         artistRepository.insert(artist);
         userArtists.add(artist);
 
-        // update user's track list
-        // with newly inserted track
         userRepository.save(user);
 
 
@@ -76,11 +71,9 @@ public class ArtistController {
 
     @GetMapping("/artists")
     public Set<Artist> getFavoriteArtists(Authentication authentication){
-        // gets current userId from token from the authorization header
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
         String currentUseId = userDetails.getId();
 
-        // finding user and returning his track set
         return userRepository.findById(currentUseId).orElseGet(User::new).getArtists();
     }
 }
