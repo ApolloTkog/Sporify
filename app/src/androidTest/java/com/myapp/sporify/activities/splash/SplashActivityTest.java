21f1a9a3eeceacc428e4222dc77e662dcc67d753
terebravisse <gnarishtnt@gmail.com>
2022-01-18 18:08:37 +0200

    I forgot about this one... I have no idea what to test atm. Will figure it out later.

---
+++ app/src/androidTest/java/com/myapp/sporify/activities/splash/SplashActivityTest.java
@@ -0,0 +1,22 @@
+package com.myapp.sporify.activities.splash;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SplashActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+}
